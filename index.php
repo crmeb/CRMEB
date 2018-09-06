44516381174b793628b41d5ea3e6681d92c6fc0a
xurongyao <rongyao_xu@163.com>
2018-09-06 16:40:41 +0800

    修改首页版本php版本

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 
 // 检测PHP环境
-if(version_compare(PHP_VERSION,'5.5.5','<'))  die('require PHP > 5.5.5 !');
+if(version_compare(PHP_VERSION,'5.5.9','<'))  die('require PHP > 5.5.9 !');
 //error_reporting(E_ALL ^ E_NOTICE);//显示除去 E_NOTICE 之外的所有错误信息
 error_reporting(E_ERROR | E_WARNING | E_PARSE);//报告运行时错误
 //检测是否已安装CrmEb系统
