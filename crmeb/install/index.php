19d9b5c102c6733ac97c2ee1c797beff90f49f9a
xurongyao <rongyao_xu@163.com>
2018-09-15 10:52:22 +0800

    移动安装目录，支持根目录index.php访问，也可以像tp5一样只对外开放crmeb一个目录

--- crmeb/install/index.php
+++ crmeb/install/index.php
@@ -27,7 +27,7 @@ define("CRMEB_VERSION", '20180601');
 date_default_timezone_set('PRC');
 error_reporting(E_ALL & ~E_NOTICE);
 header('Content-Type: text/html; charset=UTF-8');
-define('SITEDIR', _dir_path(substr(dirname(__FILE__), 0, -8)));
+define('SITEDIR', _dir_path(substr(dirname(__FILE__), 0, -14)));
 //define('SITEDIR2', substr(SITEDIR,0,-7));
 //echo SITEDIR2;
 //exit;
