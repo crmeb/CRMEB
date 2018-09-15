8149bdc42524fa82f81494769995483682655980
xurongyao <rongyao_xu@163.com>
2018-09-15 19:50:33 +0800

    移动安装目录，支持根目录index.php访问，也可以像tp5一样只对外开放crmeb一个目录

--- crmeb/index.php
+++ crmeb/index.php
@@ -24,4 +24,4 @@ if(file_exists("./install/") && !file_exists("./install/install.lock")){
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
-require __DIR__ . '/thinkphp/start.php';
+require __DIR__ . '/../thinkphp/start.php';
