825dca8eb3ceb87301aea6d75cea5b58a859653d
xurongyao <rongyao_xu@163.com>
2018-09-16 15:43:41 +0800

    移动安装目录，支持根目录index.php访问，也可以像tp5一样只对外开放public一个目录

--- application/admin/config.php
+++ application/admin/config.php
@@ -21,12 +21,12 @@ return [
     ],
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '{__ADMIN_PATH}'=>PUBILC_PATH.'/system/',
-        '{__FRAME_PATH}'=>PUBILC_PATH.'/system/frame/',
-        '{__PLUG_PATH}'=>PUBILC_PATH.'/static/plug/',
-        '{__MODULE_PATH}'=>PUBILC_PATH.'/system/module/',
-        '{__STATIC_PATH}'=>PUBILC_PATH.'/static/',
-        '{__PUBLIC_PATH}'=>PUBILC_PATH.'/'
+        '{__ADMIN_PATH}'=>PUBILC_PATH.'system/',
+        '{__FRAME_PATH}'=>PUBILC_PATH.'system/frame/',
+        '{__PLUG_PATH}'=>PUBILC_PATH.'static/plug/',
+        '{__MODULE_PATH}'=>PUBILC_PATH.'system/module/',
+        '{__STATIC_PATH}'=>PUBILC_PATH.'static/',
+        '{__PUBLIC_PATH}'=>PUBILC_PATH
     ],
     'system_wechat_tag' => '_system_wechat'
 ];
