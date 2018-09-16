46bbed6741cb932a6a4e046c60a9e07c15a6d22b
xurongyao <rongyao_xu@163.com>
2018-09-16 20:40:20 +0800

    移动安装目录，支持根目录index.php访问，也可以像tp5一样只对外开放public一个目录

--- extend/service/UploadService.php
+++ extend/service/UploadService.php
@@ -97,7 +97,6 @@ class UploadService
     {
         self::init();
         $path = self::uploadDir($path,$root);
-        print_r($path);exit;
         $dir = ROOT_PATH . $path;
         if(!self::validDir($dir)) return self::setError('生成上传目录失败,请检查权限!');
         if(!isset($_FILES[$fileName])) return self::setError('上传文件不存在!');
