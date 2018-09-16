81eadc57f6ddb9086461426d4d48d18ee98bff22
xurongyao <rongyao_xu@163.com>
2018-09-16 15:33:21 +0800

    移动安装目录，支持根目录index.php访问，也可以像tp5一样只对外开放public一个目录

--- application/admin/config.php
+++ application/admin/config.php
@@ -19,5 +19,14 @@ return [
         // 是否自动开启 SESSION
         'auto_start'     => true,
     ],
+    // 视图输出字符串内容替换
+    'view_replace_str'       => [
+        '{__ADMIN_PATH}'=>__DIR__.'/public/system/',
+        '{__FRAME_PATH}'=>__DIR__.'/public/system/frame/',
+        '{__PLUG_PATH}'=>__DIR__.'/public/static/plug/',
+        '{__MODULE_PATH}'=>__DIR__.'/public/system/module/',
+        '{__STATIC_PATH}'=>__DIR__.'/public/static/',
+        '{__PUBLIC_PATH}'=>__DIR__.'/public/'
+    ],
     'system_wechat_tag' => '_system_wechat'
 ];
