cbf2d35ed5a05c1cba9eeda1fdbf67a0286ae2ff
xurongyao <rongyao_xu@163.com>
2018-09-16 15:10:36 +0800

    移动安装目录，支持根目录index.php访问，也可以像tp5一样只对外开放public一个目录

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ if(version_compare(PHP_VERSION,'5.5.9','<'))  die('require PHP > 5.5.9 !');
 //error_reporting(E_ALL ^ E_NOTICE);//显示除去 E_NOTICE 之外的所有错误信息
 error_reporting(E_ERROR | E_WARNING | E_PARSE);//报告运行时错误
 //检测是否已安装CrmEb系统
-if(file_exists("./crmeb/install/") && !file_exists("./crmeb/install/install.lock")){
+if(file_exists("./public/install/") && !file_exists("./public/install/install.lock")){
     if($_SERVER['PHP_SELF'] != '/index.php'){
         header("Content-type: text/html; charset=utf-8");
         exit("请在域名根目录下安装,如:<br/> www.xxx.com/index.php 正确 <br/>  www.xxx.com/www/index.php 错误,域名后面不能圈套目录, 但项目没有根目录存放限制,可以放在任意目录,apache虚拟主机配置一下即可");
     }
-    header('Location:/crmeb/install/index.php');
+    header('Location:/public/install/index.php');
     exit();
 }
 // [ 应用入口文件 ]
