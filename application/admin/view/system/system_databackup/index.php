a5d28f46df8ada6be4a736864c46e84274bd5a15
xurongyao <rongyao_xu@163.com>
2018-09-17 17:27:07 +0800

    数据库备份

--- application/admin/view/system/system_databackup/index.php
+++ application/admin/view/system/system_databackup/index.php
@@ -10,7 +10,10 @@
                 <div class="layui-card-header">数据库表列表</div>
                 <div class="layui-card-body">
                     <table class="layui-hide" id="userList" lay-filter="userList"></table>
-
+                    <script type="text/html" id="barDemo">
+                        <button type="button" class="layui-btn layui-btn-xs" lay-event="edit"><i class="layui-icon layui-icon-edit"></i>编辑</button>
+                        <button type="button" class="layui-btn layui-btn-xs" lay-event="see"><i class="layui-icon layui-icon-edit"></i>详情</button>
+                    </script>
                 </div>
             </div>
         </div>
@@ -21,12 +24,14 @@
 
     layList.tableList('userList',"{:Url('tablelist')}",function () {
         return [
+            {type:'checkbox'},
             {field: 'name', title: '表名称'},
             {field: 'comment', title: '备注' },
             {field: 'engine', title: '类型'},
             {field: 'data_length', title: '大小'},
             {field: 'update_time', title: '更新时间'},
             {field: 'rows', title: '行数'},
+            {fixed: 'right', title: '操作', width: '10%', align: 'center', toolbar: '#barDemo'}
         ];
     },100);
     layList.reload();
