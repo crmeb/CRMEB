d9981d5af21290f7057e5d29cceb7b8abf63911e
xurongyao <rongyao_xu@163.com>
2018-09-18 09:21:44 +0800

    数据库备份

--- application/admin/view/system/system_databackup/index.php
+++ application/admin/view/system/system_databackup/index.php
@@ -1,24 +1,30 @@
 {extend name="public/container"}
 {block name="content"}
-<div class="layui-fluid">
-    <div class="layui-row layui-col-space15"  id="app">
-        <div class="layui-col-md12">
+<div class="row">
+    <div class="col-sm-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>数据库表列表</h5>
+            </div>
+            <div class="ibox-content" style="display: block;">
 
-        </div>
-        <div class="layui-col-md12">
-            <div class="layui-card">
-                <div class="layui-card-header">数据库表列表</div>
-                <div class="layui-card-body">
-                    <div class="layui-btn-group conrelTable">
-                        <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="set_grant"><i class="fa fa-check-circle-o"></i>备份</button>
-                        <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="optimize"><i class="fa fa-check-circle-o"></i>优化表</button>
-                        <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="repair"><i class="fa fa-check-circle-o"></i>修复表</button>
-                        <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="refresh"><i class="layui-icon layui-icon-refresh" ></i>刷新</button>
+                <div class="layui-row layui-col-space15"  id="app">
+                    <div class="layui-col-md12">
+                        <div class="layui-card">
+                            <div class="layui-card-body">
+                                <div class="layui-btn-group conrelTable">
+                                    <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="backup"><i class="fa fa-check-circle-o"></i>备份</button>
+                                    <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="optimize"><i class="fa fa-check-circle-o"></i>优化表</button>
+                                    <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="repair"><i class="fa fa-check-circle-o"></i>修复表</button>
+                                    <button class="layui-btn layui-btn-sm layui-btn-normal" type="button" data-type="refresh"><i class="layui-icon layui-icon-refresh" ></i>刷新</button>
+                                </div>
+                                <table class="layui-hide" id="userList" lay-filter="userList"></table>
+                                <script type="text/html" id="barDemo">
+                                    <button type="button" class="layui-btn layui-btn-xs" lay-event="see"><i class="layui-icon layui-icon-edit"></i>详情</button>
+                                </script>
+                            </div>
+                        </div>
                     </div>
-                    <table class="layui-hide" id="userList" lay-filter="userList"></table>
-                    <script type="text/html" id="barDemo">
-                        <button type="button" class="layui-btn layui-btn-xs" lay-event="see"><i class="layui-icon layui-icon-edit"></i>详情</button>
-                    </script>
                 </div>
             </div>
         </div>
@@ -65,6 +71,17 @@
             var tables=layList.getCheckData().getIds('name');
             if(tables.length){
                 layList.basePost(layList.Url({a:'repair'}),{tables:tables},function (res) {
+                    layList.msg(res.msg);
+//                    layList.reload();
+                });
+            }else{
+                layList.msg('请选择表');
+            }
+        },
+        backup:function () {
+            var tables=layList.getCheckData().getIds('name');
+            if(tables.length){
+                layList.basePost(layList.Url({a:'backup'}),{tables:tables},function (res) {
                     layList.msg(res.msg);
 //                    layList.reload();
                 });
