d84d32c5e1bd168bffe27b7852a72a7501214dad
sugar1569 <sugar1569@vip.qq.com>
2018-09-25 18:59:02 +0800

    数据库备份

--- application/admin/controller/system/SystemDatabackup.php
+++ application/admin/controller/system/SystemDatabackup.php
@@ -118,9 +118,9 @@ class SystemDatabackup extends AuthController
      */
     public function import(Request $request = null)
     {
-        $part = $request->get('part')?:null;
-        $start = $request->get('start')?:null;
-        $time = strtotime($request->get('time'));
+        $part = $request->post('part')?:null;
+        $start = $request->post('start')?:0;
+        $time = strtotime($request->post('time'));
         $db = $this->DB;
         if(is_numeric($time) && is_null($part) && is_null($start)){
             $list= $db->getFile('timeverif',$time);
