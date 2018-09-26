e7cf79ff246c527b9a6ca44002852c8ce7169836
sugar1569 <sugar1569@vip.qq.com>
2018-09-26 21:12:01 +0800

    样式修改

--- application/admin/view/public/container.php
+++ application/admin/view/public/container.php
@@ -8,7 +8,7 @@
     {block name="head"}{/block}
 </head>
 <body class="gray-bg">
-<div class="wrapper wrapper-content ">
+<div class="wrapper wrapper-content animated fadeInDown">
 {block name="content"}{/block}
 {block name="foot"}{/block}
 {block name="script"}{/block}
