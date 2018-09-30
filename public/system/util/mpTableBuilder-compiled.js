0c8c3871ae8e5ba8064fbf51d2deedf95b4a7404
sugar1569 <sugar1569@vip.qq.com>
2018-09-30 16:28:26 +0800

    删除过期js

--- public/system/util/mpTableBuilder-compiled.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 2017/11/3 0003.
- */
-
-//# sourceMappingURL=mpTableBuilder-compiled.js.map
--- public/system/util/mpTableBuilder.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Administrator on 2017/11/3 0003.
- */
