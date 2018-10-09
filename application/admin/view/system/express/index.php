8ffa1831d50985adfb0fb287a7b0cce64d37dd6d
sugar1569 <sugar1569@vip.qq.com>
2018-10-09 18:03:52 +0800

    添加物流公司管理

--- application/admin/view/system/express/index.php
+++ application/admin/view/system/express/index.php
@@ -4,7 +4,7 @@
     <div class="col-sm-12">
         <div class="ibox">
             <div class="ibox-title">
-                <button type="button" class="btn btn-w-m btn-primary" onclick="$eb.createModalFrame(this.innerText,'{:Url('create')}')">添加身份</button>
+                <button type="button" class="btn btn-w-m btn-primary" onclick="$eb.createModalFrame(this.innerText,'{:Url('create')}')">添加物流公司</button>
                 <div class="ibox-tools">
 
                 </div>
@@ -14,13 +14,8 @@
                     <div class="m-b m-l">
                         <form action="" class="form-inline">
 
-                            <select name="status" aria-controls="editable" class="form-control input-sm">
-                                <option value="">状态</option>
-                                <option value="1" {eq name="where.status" value="1"}selected="selected"{/eq}>显示</option>
-                                <option value="0" {eq name="where.status" value="0"}selected="selected"{/eq}>不显示</option>
-                            </select>
                             <div class="input-group">
-                                <input type="text" name="role_name" value="{$where.role_name}" placeholder="请输入身份昵称" class="input-sm form-control"> <span class="input-group-btn">
+                                <input type="text" name="keyword" value="{$params.keyword}" placeholder="请输入物流公司名称或者编码" class="input-sm form-control"> <span class="input-group-btn">
                                       <button type="submit" class="btn btn-sm btn-primary"> <i class="fa fa-search" ></i>搜索</button> </span>
                             </div>
                         </form>
@@ -33,8 +28,9 @@
                         <tr>
 
                             <th class="text-center">编号</th>
-                            <th class="text-center">身份昵称</th>
-                            <th class="text-center">权限</th>
+                            <th class="text-center">物流公司名称</th>
+                            <th class="text-center">编目</th>
+                            <th class="text-center">排序</th>
                             <th class="text-center">状态</th>
                             <th class="text-center">操作</th>
                         </tr>
@@ -46,13 +42,16 @@
                                 {$vo.id}
                             </td>
                             <td class="text-center">
-                                {$vo.role_name}
+                                {$vo.name}
                             </td>
-                            <td class="text-center" style="width: 70%;">
-                                {$vo.rules}
+                            <td class="text-center" >
+                                {$vo.code}
+                            </td>
+                            <td class="text-center" >
+                                {$vo.sort}
                             </td>
                             <td class="text-center">
-                                <i class="fa {eq name='vo.status' value='1'}fa-check text-navy{else/}fa-close text-danger{/eq}"></i>
+                                <i class="fa {eq name='vo.is_show' value='1'}fa-check text-navy{else/}fa-close text-danger{/eq}"></i>
                             </td>
                             <td class="text-center">
                                 <button class="btn btn-info btn-xs" type="button"  onclick="$eb.createModalFrame('编辑','{:Url('edit',array('id'=>$vo['id']))}')"><i class="fa fa-paste"></i> 编辑</button>
