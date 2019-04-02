654d1a322a6eec0b4b4b77d1cdbb1de391c96f51
sugar1569 <sugar1569@vip.qq.com>
2019-04-02 17:50:54 +0800

    修复安装sql

--- public/static/plug/wxApi.js
+++ public/static/plug/wxApi.js
@@ -336,7 +336,7 @@
             }
             wx.scanQRCode({
                 needResult: options.needResult || 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-                scanType: options.needResult || ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                scanType: options.scanType || ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
                 success: function (res) {
                     var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
                     successFn && successFn.call(mapleApi, result, res);
