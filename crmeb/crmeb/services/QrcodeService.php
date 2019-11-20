fdec995129ce40e4fabeb224bd5c89b679a62d41
liaofei <136327134@qq.com>
2019-11-20 16:23:02 +0800

    调整H5二维码统一由一个方法生成

--- crmeb/crmeb/services/QrcodeService.php
+++ crmeb/crmeb/services/QrcodeService.php
@@ -8,6 +8,7 @@
 namespace crmeb\services;
 
 
+use app\admin\model\system\SystemAttachment;
 use app\admin\model\wechat\WechatQrcode as QrcodeModel;
 
 class QrcodeService
@@ -37,4 +38,41 @@ class QrcodeService
     {
         return QrcodeModel::scanQrcode($id, $type);
     }
+
+    /**
+     * 获取二维码完整路径，不存在则自动生成
+     * @param string $name 路径名
+     * @param string $link 需要生成二维码的跳转路径
+     * @param int $type https 1 = http , 0 = https
+     * @param bool $force 是否返回false
+     * @return bool|mixed|string
+     */
+    public static function getWechatQrcodePath(string $name, string $link, int $type = 1, bool $force = false)
+    {
+        try {
+            $imageInfo = SystemAttachment::getInfo($name, 'name');
+            $siteUrl = sysConfig('site_url');
+            if (!$imageInfo) {
+                $codeUrl = UtilService::setHttpType($siteUrl . $link, $type);//二维码链接
+                $imageInfo = UtilService::getQRCodePath($codeUrl, $name);
+                if (is_string($imageInfo) && $force)
+                    return false;
+                if (is_array($imageInfo)) {
+                    SystemAttachment::attachmentAdd($imageInfo['name'], $imageInfo['size'], $imageInfo['type'], $imageInfo['dir'], $imageInfo['thumb_path'], 1, $imageInfo['image_type'], $imageInfo['time'], 2);
+                    $url = $imageInfo['dir'];
+                } else {
+                    $url = '';
+                    $imageInfo = ['image_type' => 0];
+                }
+            } else $url = $imageInfo['att_dir'];
+            if ($imageInfo['image_type'] == 1) $url = $siteUrl . $url;
+            return $url;
+        } catch (\Throwable $e) {
+            if ($force)
+                return false;
+            else
+                return '';
+        }
+    }
+
 }
