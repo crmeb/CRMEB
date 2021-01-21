965b23d4fc8248cf386a612bfbbb3209f7d67f56
liaofei <136327134@qq.com>
2021-01-21 14:24:58 +0800

    修复充值

--- src/crmeb/app/services/pay/PayNotifyServices.php
+++ src/crmeb/app/services/pay/PayNotifyServices.php
@@ -13,6 +13,7 @@ namespace app\services\pay;
 
 use app\services\order\OtherOrderServices;
 use app\services\order\StoreOrderSuccessServices;
+use app\services\user\UserRechargeServices;
 
 /**
  * 支付成功回调
@@ -36,6 +37,23 @@ class PayNotifyServices
             if (!$orderInfo) return true;
             if ($orderInfo->paid) return true;
             return $services->paySuccess($orderInfo->toArray(), PayServices::WEIXIN_PAY, ['trade_no' => $trade_no]);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * 充值成功后
+     * @param string|null $order_id 订单id
+     * @return bool
+     */
+    public function wechatUserRecharge(string $order_id = null, string $trade_no = null)
+    {
+        try {
+            /** @var UserRechargeServices $userRecharge */
+            $userRecharge = app()->make(UserRechargeServices::class);
+            if ($userRecharge->be(['order_id' => $order_id, 'paid' => 1])) return true;
+            return $userRecharge->rechargeSuccess($order_id);
         } catch (\Exception $e) {
             return false;
         }
